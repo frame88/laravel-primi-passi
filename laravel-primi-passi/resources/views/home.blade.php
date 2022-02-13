72c5f0ee5e2836801093239c6c8ff0247df5a381
frame88 <fleuzzi97@gmail.com>
2022-02-13 17:22:43 +0100

    base camp

---
+++ laravel-primi-passi/resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel primi passi</title>
+</head>
+<body>
+{{-- 
+Cancelliamo la view welcome.blade.php e creiamo una nostra homepage.
+Facciamo quindi sì che la rotta / visualizzi home.blade.php
+Inizialmente stampiamo un Hello World, poi passiamo dei dati alla view in modo da visualizzarli dinamicamente con Blade. 
+--}}
+
+    <h1>Main page</h1>
+</body>
+</html>
